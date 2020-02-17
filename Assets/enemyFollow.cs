e1cd644e8bfc2fc53c067ca88852d4e476114ac5
ocasusMaximus <za.jan@seznam.cz>
2020-02-17 13:50:16 +0100

    commit of unity test
    first commit of Unity

---
+++ Assets/enemyFollow.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyFollow : MonoBehaviour
+{
+
+    public Transform target;
+    int MoveSpeed = 4;
+    int MaxDist = 10;
+    int MinDist = 2;
+    private CharacterController controller;
+    private float verticalVelocity;
+    private float jumpForce = 2f;
+    private float gravity = 3f;
+    Vector3 currentPos;
+ 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+
+
+            if (controller.isGrounded)
+            {
+                verticalVelocity = -gravity * Time.deltaTime;
+
+                if (Vector3.Distance(transform.position, target.position) < MinDist)
+                {
+                    
+                    verticalVelocity = jumpForce;
+                transform.position = currentPos;
+                }
+            
+            
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+      
+
+
+        Vector3 moveVector = new Vector3(0, verticalVelocity, 0);
+        controller.Move(moveVector * Time.deltaTime);
+
+
+        if (Vector3.Distance(transform.position, target.position) > MinDist)
+        {
+
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+            currentPos = transform.position;
+
+        }
+
+
+
+    }
+}
