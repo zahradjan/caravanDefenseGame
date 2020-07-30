8c936bad22f0103196bd81913790a766352b51f4
xmannm <xmannm125@gmail.com>
2020-07-30 21:19:53 +0200

    začátek Skills UI

---
+++ Assets/Scripts/EquipmentWindow/SkillsUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillsUI : MonoBehaviour
+{
+
+    public GameObject skillsUI;
+
+    public void EnableSkillsUI()
+    {
+        //skillsUI.SetActive(!skillsUI.activeSelf);
+        skillsUI.SetActive(true);
+
+    }
+
+    public void DisableSkillsUI()
+    {
+     skillsUI.SetActive(false);
+
+    }
+
+}
