07b00d3aae91c528bec0ba2543f60aa5c5c2ff01
xmannm <xmannm125@gmail.com>
2020-08-17 14:19:01 +0200

    přidání Resources systému
    Lvl Up teď stojí 'Resources', který hráč musí nejprve lootnout
    (v lootu je item Infinite Bag of Gold pro testování)

--- Assets/Scripts/EquipmentWindow/Item.cs
+++ Assets/Scripts/EquipmentWindow/Item.cs
@@ -7,12 +7,21 @@ public class Item : ScriptableObject{
     new public string name = "New Item";    
     public Sprite icon = null;              
     public bool isDefaultItem = false;
+    public bool isJustResources;
+    public int resourcesValue;
+
 
     public virtual void Use ()
     {
-        //use the item (also works for other items then equipment like consumables or something)
         Debug.Log("Using " + name);
         //override in Equipment.cs
+
+        if (isJustResources == true)
+        {
+            Resources.instance.AddResources(resourcesValue);
+            //RemoveFromInventory();
+        }
+        
     }
 
     public void RemoveFromInventory ()
