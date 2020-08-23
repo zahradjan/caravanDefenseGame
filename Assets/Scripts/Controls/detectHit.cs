ef39ef5680318a6260b15efe8440e3888540a77b
ocasusMaximus <za.jan@seznam.cz>
2020-08-23 15:24:20 +0200

    Better collision detecking and some refactors

--- Assets/Scripts/Controls/detectHit.cs
+++ Assets/Scripts/Controls/detectHit.cs
@@ -20,17 +20,22 @@ public class detectHit : MonoBehaviour
         // hit ktery dostava i kdyz ten nepritel spadne k zemi je proto ze kollidery toho nepritele se vypnou protoze je mrtvej
         // => cili ta podminka se provede problem je ze collidery toho zivyho furt detekuji kolizi a tim padem ten hit z toho
         // padu detekuji a proto se ty zivoty uberou :)
-        if (anim.GetBool("isDead") == true) return;
+        //if (anim.GetBool("isDead") == true) return;
+
+        //if (other.tag == transform.tag) return;
 
 
         //Debug.Log("My layer: " + gameObject.layer);
         //Debug.Log("Opponents layer: " + other.gameObject.layer);
         //Debug.Log("My tag: " + gameObject.tag);
         //Debug.Log("Opponents tag: " + other.gameObject.tag);
-
-
-        
+        if(other.tag != transform.tag && anim.GetBool("isDead") == false)
+        {
             DealDamage();
+        }
+
+
+      
 
        
     
@@ -49,12 +54,17 @@ public class detectHit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (currentHealth <= 0)
+        if (MyTimeHasCome())
         {
             Die();
         }
     }
-        
+
+    private bool MyTimeHasCome()
+    {
+        return currentHealth <= 0 && gameObject.layer != 11;
+    }
+
     int GetCurrentHealth()
     {
         return currentHealth;
@@ -73,16 +83,21 @@ public class detectHit : MonoBehaviour
     }
     void Die()
     {
-            // tady jsem skoncil takhle neprovadet on se pri kazdem updatu bude hazet do ty vrstvy a bude se to furt provolavat
-            anim.SetBool("isDead", true);
-        if (gameObject.layer != 11)
-        {
-            gameObject.layer = LayerMask.NameToLayer("Hell");
-            transform.Find("Armature/ik_wrist_right/Capsule").gameObject.layer = LayerMask.NameToLayer("Hell");
-            transform.Find("HealthBar").gameObject.SetActive(false);
-            //transform.gameObject.SetActive(false);
-        }
-       
+     
       
-        }
+        
+       
+            Debug.Log("Umrel jsem!");
+            anim.SetBool("isAttacking", false);
+            anim.SetBool("isDead", true);
+            gameObject.layer = LayerMask.NameToLayer("Hell");
+            
+            transform.Find("HealthBar").gameObject.SetActive(false);
+            transform.Find("Armature/ik_wrist_right/right_hand/weapon_bone/Collider").gameObject.SetActive(false);
+            gameObject.GetComponent<CapsuleCollider>().enabled = false;
+       
+        
+
+
+    }
 }
