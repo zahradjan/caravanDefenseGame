e34dd45957ec4b2045ee5ce0b5eacd7ef9b50dbb
ocasusMaximus <za.jan@seznam.cz>
2020-09-19 16:11:28 +0200

    Grid update

---
+++ Assets/TemplateScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemplateScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject finalObject;
+
+    private Vector3 mousePos;
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,0f)));
+        transform.position = new Vector3(Mathf.Round(mousePos.x), Mathf.Round(mousePos.y), 0f);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("POSX: " + mousePos.x);
+            Debug.Log("POSY: " + mousePos.y);
+            Debug.Log("POSeX: " + transform.position.x);
+            Debug.Log("POSeY: " + transform.position.y);
+            Instantiate(finalObject, transform.position, Quaternion.identity);
+        }
+    }
+}
