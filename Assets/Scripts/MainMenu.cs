3206d10d96314333dc5875932557ef8a19a68253
John Gardener <za.jan@seznam.cz>
2021-08-08 22:20:34 +0200

    Crossfade transition added

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class MainMenu : MonoBehaviour
 {
+    public Animator transition;
+    public float transitionTime = 1f;
   public void SetScene(int sceneNumber)
     {
-        SceneManager.LoadScene(sceneNumber);
+        StartCoroutine(LoadScene(sceneNumber));
+       
     }
 
     public void QuitGame()
     {
         Debug.Log("QUIT!!!");
         Application.Quit();
+    }
+    IEnumerator LoadScene(int sceneNumber){
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+         SceneManager.LoadScene(sceneNumber);
     }
 
 }
